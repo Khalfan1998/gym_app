974e74391be6c6be2bd614be3df8fc15e8bc8706
Khalfan1998 <khalfan.ikns@gmail.com>
2022-06-29 15:34:10 +0300

    adding exercise detail

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useSyncExternalStore } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
